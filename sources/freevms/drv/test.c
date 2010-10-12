46c8c857134ef2e482b9d1ba17038e7ee8f87c64
BERTRAND Joel <joel.bertrand@systella.fr>
2010-10-12 17:30:32 +0200

    New files

---
+++ sources/freevms/drv/test.c
@@ -0,0 +1,8 @@
+#include "l4io.h"
+
+int
+main(void)
+{
+	printf("Hello, World !\n");
+	return(0);
+}
---
+++ sources/freevms/root/system.h
@@ -0,0 +1,2 @@
+#define RUN_S_PROC_ID		"%%RUN-S-PROC_ID, "
+#define RUN_S_PRIVILEGES	"%%RUN-S-PRIVILEGES, "
