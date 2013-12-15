96030cf44ab311bd6d40accadc01995581ae8bbb
BERTRAND Joël <joel.bertrand@systella.fr>
2013-12-15 11:58:15 +0100

    Changing freetype include directory.

--- sources/bootloader/util/grub-mkfont.c
+++ sources/bootloader/util/grub-mkfont.c
@@ -39,7 +39,7 @@
 #include FT_FREETYPE_H
 #include FT_TRUETYPE_TAGS_H
 #include FT_TRUETYPE_TABLES_H
-#include <freetype/ftsynth.h>
+#include <freetype2/ftsynth.h>
 
 #undef __FTERRORS_H__
 #define FT_ERROR_START_LIST   const char *ft_errmsgs[] = { 
